cbfff0c08d60b82feeaf61e003a9d6e8ac7ad41e
sean <spcfox2@gmail.com>
2017-07-14 01:02:42 +1000

    renamed and cleaned up old fastfood implementation

---
+++ src/main/scala/systolic/linear/fastfoodExp.scala
@@ -0,0 +1,161 @@
+package linear
+
+import Chisel._
+import com.github.tototoshi.csv._
+import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable.ListBuffer
+import scala.util.Random
+
+import utils._
+
+import math._
+
+/*
+Fastfood Experimental Version:
+  - Does not implement FWHT
+  - Generates the Hadamard coeffs, and computes in O(nd) time
+  - Linear Array, with two overlapping datapaths
+  - not working for n==p 
+*/
+
+class CtrlIO extends Bundle{
+
+  val prst = Bool(OUTPUT)
+  val pload = Bool(OUTPUT)
+  //val pinc = Bool(OUTPUT)
+  val padd = Bool(OUTPUT)
+  val dload = Bool(OUTPUT)
+  val dadd = Bool(OUTPUT)
+  val func = UInt(OUTPUT, 4)
+
+}
+
+class LFSR( val init : Int ) extends Module{
+  val io = new Bundle{
+    val en = Bool(INPUT)
+    val rst = Bool(INPUT)
+    val out = Bool(OUTPUT)
+  }
+  val res = RegInit( UInt(init, 16) )
+  val nxt_res = Cat( res(0)^res(2)^res(3)^res(5), res(15,1) )
+  when( io.en ){
+    res := nxt_res
+  }
+
+  when( io.rst ){
+    res := UInt( init, 16 )
+  }
+
+  // 1-bit output
+  io.out := res(0)
+
+}
+
+class Fastfood( val bitWidth : Int, val fracWidth : Int, 
+    val n : Int, val p : Int, val d : Int, g : Seq[Seq[BigInt]], 
+    s : Seq[Seq[BigInt]], alpha : Seq[Seq[BigInt]], val aStages : Int,
+    cosFlag : Boolean = false, forSim : Boolean = true ) extends Module {
+  
+  val io = new Bundle{
+    val inData = Decoupled( Vec.fill(2){ Fixed(INPUT, bitWidth, fracWidth) } ).flip
+    val outData = Valid( Fixed(OUTPUT, bitWidth, fracWidth) )
+  }
+
+  Predef.assert( n>p, "Error: Only working for N>P")
+  Predef.assert( p>1, "Error: P must be greater than 1")
+  Predef.assert( (d & (d - 1)) == 0, "Error: X dimensionality is NOT a power of 2")
+  Predef.assert( p<=d, "Error: Number of PE's should not be greater than dimensionality")
+  Predef.assert( (p & (p-1)) == 0, "Error: P is NOT a power of 2" )
+  Predef.assert( (n & (n-1)) == 0, "Error: N is NOT a power of 2" ) // required for this implemtation
+  Predef.assert( n/p <= d, "Error: A PE can NOT compute partial WHT" )
+  Predef.assert( (g.length == s.length) & (g.length== alpha.length), "Error: Parameter lengths are NOT equal")
+  Predef.assert( g.length == p, "Error: Inconsistent parameter dimensions")
+  val j = (d/p) // number of loads required for one x-vector
+  Predef.assert( (j & (j - 1)) == 0, "Error: the number of X loads must be a power of 2")
+
+
+  val array = (0 until p).reverse.map( i => Module( new PE(i, bitWidth, fracWidth, 
+                                                    n, p, d, g(i), s(i), alpha(i), 
+                                                    aStages, cosFlag, forSim ) ) )
+
+  val fsm = Module( new FSM( bitWidth, fracWidth, n, p, d, aStages, cosFlag ) )
+
+  val fifoDepth = d*10
+  val readyToStart = Bool()
+  val inFifo = Module( new Queue( Vec.fill(2){ Fixed( width=bitWidth, fracWidth=fracWidth ) }, fifoDepth ) ) 
+
+  readyToStart := ( inFifo.io.count >= UInt( p, width=log2Up(p)+1 ) )
+
+  inFifo.io.enq.bits := io.inData.bits
+  inFifo.io.enq.valid := io.inData.valid
+  inFifo.io.deq.ready := ( readyToStart && fsm.io.xrdy )
+  
+  // FSM inputs
+  fsm.io.vld := readyToStart
+
+  // connect array
+  array(0).io.pin := array(p-1).io.pout 
+  array(0).io.sin := array(p-1).io.sout   
+  array(0).io.xin := RegNext( RegNext( inFifo.io.deq.bits(0) ) ) //fsm.io.xout
+  array(0).io.yin := RegNext( RegNext( inFifo.io.deq.bits(1) ) ) //fsm.io.yout //Fixed(toFixed(-4.56, fracWidth), bitWidth, fracWidth ) ) ) //
+  for (ix <- 1 until p){
+    array(ix).io.pin := array(ix-1).io.pout
+    array(ix).io.sin := array(ix-1).io.sout
+    array(ix).io.xin := array(ix-1).io.xout
+    array(ix).io.yin := array(ix-1).io.yout
+  }
+  // connect the hadamard loopback:
+  val hadArrays = array.grouped( d*p/n ).toList
+  val ld = d*p/n  // number of PEs per hadamard
+  for( ix <- 0 until hadArrays.length ){
+    hadArrays(ix)(0).io.hin := hadArrays(ix)(ld-1).io.hout
+    for( iy <- 1 until ld){
+      hadArrays(ix)(iy).io.hin := hadArrays(ix)(iy-1).io.hout
+    }
+  }
+
+  val ctrlFanOut_0 = RegNext( fsm.io.ctrl )
+  val ctrlFanOut_1 = RegNext( ctrlFanOut_0 )
+
+  // control signals
+  for( ix <- 0 until p){
+    array( ix ).io.ctrl := ctrlFanOut_1
+  }
+
+  io.inData.ready := inFifo.io.enq.ready
+  io.outData.bits := array(0).io.sout 
+  io.outData.valid := fsm.io.yrdy
+
+}
+
+object fastfoodVerilog{
+
+  val bitWidth = 32 //24
+  val fracWidth = 24 //16
+  val n = 128 //16
+  val p = 32 //4
+  val d = 64 //8
+  val sigma = 11.47
+  val aStages = 6
+
+  // get n-length parameter vectors
+  val (gvec, svec, avec) = helper.fit( n, d, sigma )
+  
+  // convert to fixed point, and group to PEs
+  val g = gvec.map(x => toFixed(x, fracWidth) ).grouped( n/p ).toVector
+  val s = svec.map(x => toFixed(x, fracWidth) ).grouped( n/p ).toVector
+  val alpha = avec.map(x => toFixed(x, fracWidth) ).grouped( n/p ).toVector
+
+  println( g )
+  println( s )
+  println( alpha )
+
+  def main(args: Array[String]): Unit = {
+    println(s"Generating verilog for the $n/$d/$p array of PEs")
+    chiselMain(Array("--backend", "v", "--targetDir", "verilog"), 
+                () => Module( new Fastfood( bitWidth, fracWidth, n, p, d, g, s, alpha, 
+                  aStages, true, false ) ) )
+  }
+
+
+}
---
+++ src/main/scala/systolic/linear/fastfoodExpTester.scala
@@ -0,0 +1,145 @@
+package linear
+
+import Chisel._
+import com.github.tototoshi.csv._
+import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable.ListBuffer
+import scala.util.Random
+
+// import breeze library stuff: https://github.com/scalanlp/breeze/wiki
+import breeze.stats.distributions._
+import breeze.linalg._
+import breeze.numerics._
+import breeze.math._
+
+import scala.collection.immutable.Vector //to override breeze.Vector
+
+import utils._
+
+
+
+class fastfoodSim( c: Fastfood ) extends Tester( c ){
+
+  val n = c.n
+  val d = c.d
+  val p = c.p
+  val fracWidth = c.fracWidth
+  val bitWidth = c.bitWidth
+
+  var k = 0
+
+  val (xdat, ydat) = helper.genData(d, 10, 2, 10000, 20)
+  println( xdat.length )
+  println( xdat(0).length ) 
+  println( ydat.length )
+
+  val rng = new Random( 33 )
+
+  poke( c.io.inData.valid, true )
+
+  // just the first 4 training examples
+  for( ix <- 0 until 4 ){
+    poke( c.io.inData.bits(1), toFixed( ydat(ix), fracWidth ) )
+    for( iy <- 0 until d ){
+      poke(c.io.inData.bits(0), toFixed( xdat(ix)(iy), fracWidth ) )
+      step(1)
+    }
+  }
+  step(450)
+
+  /*
+  for( ix <- 0 until d*2 ){
+    poke( c.io.inData.bits(0), toFixed( rng.nextFloat, fracWidth ) )
+    step(1)
+    peek( c.io.outData.bits )
+  }
+  step(450)
+  */
+
+}
+
+object helper{
+
+  def fit(n : Int, d : Int, sigma : Double) : (Vector[Double], Vector[Double], Vector[Double]) = {
+
+    implicit val basis : RandBasis = RandBasis.withSeed(0)
+    val rng1 = new Gaussian(0, 1)
+    val rng2 = new ChiSquared( d )
+
+    // original gmat and smat
+    var gmat = DenseMatrix.rand( n/d, d, rng1 )
+    var smat = DenseMatrix.rand( n/d, d, rng2 )
+
+    //calculate the frobenius norm of G, (||G||_frob)^1/2
+    //val frob = sqrt( sum( (g *:* g).copy(*, ::) ) ) // multiply elementwise, then sum across each row
+    val frob = norm( gmat(*, ::) ) // frobenius norm of each row   
+    smat = smat(::, *) *:* (1.0 /:/ frob)
+    smat = ( 1.0/( sigma*sqrt(d) ) ) *:* smat
+
+    //get equivalent n-length g,s,alpha vectors
+    ( gmat.t.toDenseVector.toArray.toVector,
+      smat.t.toDenseVector.toArray.toVector,
+      rng1.sample( n ).toVector )
+
+  }
+
+  def genData( d : Int = 8, lookAhead : Int = 10, step : Int = 2, 
+      n_samples : Int = 10000, n_rate : Int = 20 ) : (Array[Array[Double]], Array[Double]) = {
+    // generate a basic dataset for modeling a Sine Wave
+    //    # d - number of features
+    //    # lookAhead - how far ahead to predict
+    //    # step - sliding window step
+    //    # n_samples - size of raw sine data
+    //    # n_rate - how many samples per radian, pi*radians in a sine period
+
+    val xraw = linspace(0, n_samples/n_rate, n_samples)
+    val xsine = sin(xraw).toArray
+    val ydat = xsine.sliding(d+lookAhead, step).toArray.map( x => x.last ) 
+ 
+    ( xsine.sliding(d, step).toArray.take( ydat.length ), ydat)
+
+  }
+
+}
+
+
+object fastfoodTester{
+
+  val bitWidth = 18
+  val fracWidth = 10
+
+  val sigma = 11.47
+  val n = 16
+  val p = 4 
+  val d = 8
+
+  // alu stages
+  val aStages = 4
+
+  // get n-length parameter vectors
+  val (gvec, svec, avec) = helper.fit( n, d, sigma )
+  
+  // convert to fixed point, and group to PEs
+  val g = gvec.map(x => toFixed(x, fracWidth) ).grouped( n/p ).toVector
+  val s = svec.map(x => toFixed(x, fracWidth) ).grouped( n/p ).toVector
+  val alpha = avec.map(x => toFixed(x, fracWidth) ).grouped( n/p ).toVector
+
+  println( g )
+  println( s )
+  println( alpha )
+
+
+  def main(args: Array[String]): Unit = {
+    println("Testing the FSM for controlling the array")
+    
+    chiselMainTest(Array("--genHarness", "--test", "--backend", "c", //"--wio", 
+      "--compile", "--targetDir", ".emulator", "--vcd"), // .emulator is a hidden directory
+      () => Module(new Fastfood( bitWidth, fracWidth, n, p, d, g, s, 
+                        alpha, aStages, false, true ) ) ) {
+        f => new fastfoodSim( f )
+      }
+  }
+}
+
+//not working for d==p, and also if aStages<3 and cosFlag=true, and this is because the
+// alu stages is less than mStages.
